5de4dbdba5ec89d8ddc19e95db1890a08ddb8870
Lee <koioriy@qq.com>
2018-11-21 16:25:16 +0800

    Update selenium-第一课.py

--- selenium-第一课.py
+++ selenium-第一课.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 for i in range(3):
     web=webdriver.Firefox()
     web.get("https:\\www.baidu.com")
+    web.find_element_by_id("kw").send_keys("aaaa")
 print(web.title)
