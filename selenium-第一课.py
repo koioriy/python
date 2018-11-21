3e5b5e1e66f396932519e07103fe238910a4eb6a
koioriy <koioriy@qq.com>
2018-11-21 16:30:22 +0800

    打开三个网页

--- selenium-第一课.py
+++ selenium-第一课.py
@@ -3,4 +3,5 @@ for i in range(3):
     web=webdriver.Firefox()
     web.get("https:\\www.baidu.com")
     web.find_element_by_id("kw").send_keys("aaaa")
+    web.find_element_by_id("su").click()
 print(web.title)
