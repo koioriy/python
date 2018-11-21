021277beb86000525c51364fb7606db85e5a8dc3
koioriy <koioriy@qq.com>
2018-11-21 16:32:08 +0800

    增加按钮行为

--- selenium-第一课.py
+++ selenium-第一课.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-for i in range(3):
+for i in range(1):
     web=webdriver.Firefox()
     web.get("https:\\www.baidu.com")
     web.find_element_by_id("kw").send_keys("aaaa")
     web.find_element_by_id("su").click()
+    web.quit()
 print(web.title)
